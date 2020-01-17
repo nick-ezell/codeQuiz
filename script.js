9e49fde70ab182e9d85bdf4eadca4a4beea68439
Nick Ezell <alexsandratrammell@Nicks-MacBook-Pro.local>
2020-01-17 14:56:29 -0600

    Restructured array of questions to make them more usable in a loop.

--- script.js
+++ script.js
@@ -2,42 +2,33 @@ const startQuiz = {
     text: "In this quiz you will test your knowledge of basic coding information. You will have one minute, press the button to start.",
     button: "Start Quiz"
 }
-const questions = {
-    question1: {
+const questions = [
+    question1 = {
         question: "Of the choices, which is not one of the three data types in JavaScript?",
-        correct: "Loops",
-        choice2: "String",
-        choice3: "Number",
-        choice4: "Boolean"
+        choices: ["Loops", "String", "Number", "Boolean"],
+        answer: "Loops"
     },
-    question2: {
+    question2 = {
         question: "What does 'HTML' stand for?",
-        choice1: "Hyper Text Making Language", 
-        choice2: "Href Type Model Listener",
-        correct: "Hyper Text Markup Language", 
-        choice4: "Happy Trex Make Loud"
+        choices: ["Hyper Text Making Language", "Href Type Model Listener", "Hyper Text Markup Language", "Happy Trex Make Loud"],
+        answer: "Hyper Text Markup Language"
     },
-    question3: {
+    question3 = {
         question: "What does () indicate?",
-        choice1: "String",
-        choice2: "If statement", 
-        correct: "Method",
-        choice4: "Variable"
+        choices: ["String", "If statement", "Method", "Variable"],
+        answer: "Method" 
     },
-    question4: {
+    question4 = {
         question: "What does 'DOM' stand for?",
-        choice1: "Dragon Operated Missile",
-        choice2: "Document Oriented Markup", 
-        choice3: "Data Object Model",
-        correct: "Document Object Model"
+        choices: ["Dragon Operated Missile", "Document Oriented Markup", "Data Object Model", "Document Object Model"],
+        answer: "Document Object Model"
     }
-}
+]
 let highScores = [];
-let firstQ = questions.question1;
-let secondQ = questions.question2;
-let thirdQ = questions.question3;
-let fourthQ = questions.question4;
-let container = document.querySelector(".container");
+let firstQ = questions[0];
+let secondQ = questions[1];
+let thirdQ = questions[2];
+let fourthQ = questions[3];
 let qText = document.querySelector("#text");
 let result = document.querySelector("#result");
 let timeLeft = document.querySelector("#timeLeft");
@@ -51,6 +42,7 @@ function setTime() {
       }
     }, 1000);
   }
+  
   window.addEventListener("load", function start(){
     var startText = startQuiz.text;
     var startBtn = startQuiz.button;
