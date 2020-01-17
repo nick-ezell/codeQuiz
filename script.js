bed239fb4dc270acc3c41fafbe90046ed3355a2d
Nick Ezell <alexsandratrammell@Nicks-MacBook-Pro.local>
2020-01-17 01:45:11 -0600

    Wrote HTML/did basic page CSS as well as created JS objects. Declared variables and set timer for quiz.

---
+++ script.js
@@ -0,0 +1,52 @@
+const startQuiz = {
+    text: "In this quiz you will test your knowledge of basic coding information.",
+    button: "Start Quiz"
+}
+const questions = {
+    question1: {
+        question: "Of the choices, which is not one of the three data types in JavaScript?",
+        correct: "Loops",
+        choice2: "String",
+        choice3: "Number",
+        choice4: "Boolean"
+    },
+    question2: {
+        question: "What does 'HTML' stand for?",
+        choice1: "Hyper Text Making Language", 
+        choice2: "Href Type Model Listener",
+        correct: "Hyper Text Markup Language", 
+        choice4: "Happy Trex Make Loud"
+    },
+    question3: {
+        question: "What does () indicate?",
+        choice1: "String",
+        choice2: "If statement", 
+        correct: "Method",
+        choice4: "Variable"
+    },
+    question4: {
+        question: "What does 'DOM' stand for?",
+        choice1: "Dragon Operated Missile",
+        choice2: "Document Oriented Markup", 
+        choice3: "Data Object Model",
+        correct: "Document Object Model"
+    }
+}
+let firstQ = questions.question1;
+let secondQ = questions.question2;
+let thirdQ = questions.question3;
+let fourthQ = questions.question4;
+let qText = document.querySelector("#text");
+let result = document.querySelector("#result");
+let timeLeft = document.querySelector("#timeLeft");
+let counter = 60;
+function setTime() {
+    var timerInterval = setInterval(function() {
+      counter--;
+      timeLeft.textContent = counter;
+      if(counter===0){
+          clearInterval(timerInterval);
+      }
+    }, 1000);
+  }
+  setTime();
